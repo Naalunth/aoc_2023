4860026146b2d8d7886704511c5ff97d84b3b10f
Lilith Schier <lilith@naalunth.dev>
2023-12-01 12:27:24 +0100

    formatted

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -2,9 +2,11 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day1, part1)]
 pub fn part_1(input: &str) -> u64 {
-    input.lines()
+    input
+        .lines()
         .map(|line| {
-            let digits = line.bytes()
+            let digits = line
+                .bytes()
                 .filter(|c| c.is_ascii_digit())
                 .map(|c| c - b'0');
             let first_digit = digits.clone().next().unwrap();
@@ -14,10 +16,10 @@ pub fn part_1(input: &str) -> u64 {
         .sum()
 }
 
-
 #[aoc(day1, part2)]
 pub fn part_2(input: &str) -> u64 {
-    input.lines()
+    input
+        .lines()
         .map(|line| {
             let first_digit = word_to_digit(first_match(line, &VALID_MATCHES));
             let last_digit = word_to_digit(last_match(line, &VALID_MATCHES));
@@ -27,8 +29,8 @@ pub fn part_2(input: &str) -> u64 {
 }
 
 const VALID_MATCHES: [&str; 19] = [
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+    "seven", "eight", "nine",
 ];
 
 fn word_to_digit(word: &str) -> u8 {
@@ -50,7 +52,7 @@ fn word_to_digit(word: &str) -> u8 {
 fn first_match<'a>(haystack: &str, words: &[&'a str]) -> &'a str {
     for idx in 0..haystack.bytes().len() {
         for &word in words {
-            if haystack[idx..].starts_with(word) {
+            if haystack.as_bytes()[idx..].starts_with(word.as_bytes()) {
                 return word;
             }
         }
@@ -61,14 +63,13 @@ fn first_match<'a>(haystack: &str, words: &[&'a str]) -> &'a str {
 fn last_match<'a>(haystack: &str, words: &[&'a str]) -> &'a str {
     for idx in (0..haystack.bytes().len()).rev() {
         for &word in words {
-            if haystack[idx..].starts_with(word) {
+            if haystack.as_bytes()[idx..].starts_with(word.as_bytes()) {
                 return word;
             }
         }
     }
     panic!("No word found in: {}", haystack);
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -77,11 +78,14 @@ mod tests {
     #[test]
     fn part_1_test() {
         assert_eq!(
-            part_1("1abc2
+            part_1(
+                "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet"),
-            142);
+treb7uchet"
+            ),
+            142
+        );
     }
 
     #[test]
